f9506a424bf361836f7aed63a7a9dc0f6c6aec9b
Ahmad <ahmadpadarwala4@gmail.com>
2023-08-29 16:13:58 +0500

    Create topbar in client side

--- client/src/component/Navbar.js
+++ client/src/component/Navbar.js
@@ -1,9 +1,39 @@
 import React from "react";
 import "../assets/css/Navbar.css";
+import { NavLink } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <>
-      <h1>This is a Navbar</h1>
+      <div className="w-full header-topbar-main">
+        <div className="header-topbar-sections">
+          <div>
+            <i className="fa-solid fa-phone me-2"></i>
+            <span>Phone: 73832 94925</span>
+          </div>
+          <div>
+            <i className="fa-solid fa-envelope me-2"></i>
+            <span>Email: ahmadpadarwala4@gmail.com</span>
+          </div>
+        </div>
+        <div className="header-topbar-sections">
+          <span>Follow us: </span>
+          <span className="topbar-icons">
+            <NavLink to="/" className="topbar-icon-link">
+              <i className="fa-brands fa-facebook-f"></i>
+            </NavLink>
+            <NavLink to="/" className="topbar-icon-link">
+              <i className="fa-brands fa-x-twitter"></i>
+            </NavLink>
+            <NavLink to="/" className="topbar-icon-link">
+              <i className="fa-brands fa-linkedin-in"></i>
+            </NavLink>
+            <NavLink to="/" className="topbar-icon-link">
+              <i className="fa-brands fa-instagram"></i>
+            </NavLink>
+          </span>
+        </div>
+      </div>
     </>
   );
 };
