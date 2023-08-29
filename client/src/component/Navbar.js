53d5801c2a5dd9feb2af3dedfc3c7682867ee076
Ahmad <ahmadpadarwala4@gmail.com>
2023-08-29 19:00:31 +0500

    Navbar complete without responsive in ahmad

--- client/src/component/Navbar.js
+++ client/src/component/Navbar.js
@@ -5,7 +5,7 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <>
-      <div className="w-full header-topbar-main">
+      <div className="header-topbar-main">
         <div className="header-topbar-sections">
           <div>
             <i className="fa-solid fa-phone me-2"></i>
@@ -34,6 +34,48 @@ const Navbar = () => {
           </span>
         </div>
       </div>
+
+      <header>
+        <nav className="fixed w-full bg-white shadow-md px-10">
+          <div className="flex items-center px-6 py-4 justify-between">
+            <div className="flex items-center space-x-4">
+              <NavLink to="/">
+                <img
+                  src={require("../assets/image/logo.webp")}
+                  alt="logo"
+                  width="144px"
+                  height="auto"
+                />
+              </NavLink>
+            </div>
+            <ul className="flex space-x-6">
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/">About</NavLink>
+              </li>
+              <li>
+                <NavLink to="/">Services</NavLink>
+              </li>
+              <li>
+                <NavLink to="/">Contact</NavLink>
+              </li>
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+            </ul>
+            <div>
+              <NavLink to="/" className="navbar-button">
+                Wants To Visit?
+              </NavLink>
+            </div>
+          </div>
+        </nav>
+      </header>
     </>
   );
 };
