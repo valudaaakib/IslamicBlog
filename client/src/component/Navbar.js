21d1de1ea96ce6c614fe4ee4246cc93337438b86
Ahmad <ahmadpadarwala4@gmail.com>
2023-08-29 14:13:47 +0500

    Complete folder structure

---
+++ client/src/component/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../assets/css/Navbar.css";
+const Navbar = () => {
+  return (
+    <>
+      <h1>This is a Navbar</h1>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ client/src/routes/HomeRoute.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+const HomeRoute = () => {
+  return <div></div>;
+};
+
+export default HomeRoute;
