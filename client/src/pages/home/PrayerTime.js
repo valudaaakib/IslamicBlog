41b015f384160e0aff1fc520bfab62c1c1a8c84d
Amil <patelaamil509@gmail.com>
2023-08-30 14:56:46 +0530

    four piller are almost completed in amil branch

--- client/src/pages/home/PrayerTime.js
+++ client/src/pages/home/PrayerTime.js
@@ -4,9 +4,9 @@ import '../../assets/css/pages/Home.css';
 const PrayerTime = () => {
     return (
         <>
-            <div className='flex flex-wrap'>
-                <div className='salah-right-section'>
-                    <h2 className='text-white text-4xl pt-20 text-center'>PRAYER TIMES</h2>
+            <div className='main-prayer-section'>
+                <div className='salah-left-section'>
+                    <h2 className='text-white text-4xl pt-10 text-center'>PRAYER TIMES</h2>
                     <p className='text-center pt-5 text- font-bold text-base hover:text-white'>Prayer times in Jakarta</p>
                     <p className='text-center text-white pt-3'>Tue 29 Aug</p>
                     <table className='mx-auto'>
@@ -63,7 +63,7 @@ const PrayerTime = () => {
                     </table>
                     <p className='text-center pt-5'>Powerd by <span className='hover:text-white'>Muslim Pro</span></p>
                 </div>
-                <div className='salah-left-section'>
+                <div className='salah-right-section'>
                     <p className='prayer-right-header'>Welcome to the Islamic Center</p>
                     <p className='salah-left-peragraph'>The Shaha is not just a mosque for prayers rather it is a community center for all.
                         The Center is committed to preserving an Islamic identity, building and
